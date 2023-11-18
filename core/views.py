bd7629527c8e4fddd9956f560e3bda05006c79c6
MrPatito31 <tomixd@gmail.com>
2023-11-18 18:28:02 -0300

    primer template/vista (login)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'core/index.html'),
+    return render(request, 'core/index.html')
 
 def login(request):
-    return render(request, 'core/login.html'),
+    return render(request, 'core/login.html')
